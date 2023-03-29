41011532dafbf50e8781db50012c5d5439eb5d82
xiplus <huangxuanyuxiplus@gmail.com>
2023-03-29 21:10:16 +0800

    Initial commit

---
+++ edit.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+import argparse
+import logging
+import os
+import sys
+
+os.environ['PYWIKIBOT_DIR'] = os.path.dirname(os.path.realpath(__file__))
+import pywikibot
+import pywikibot.pagegenerators
+
+
+class GererateLtaList:
+    def __init__(self, args):
+        self.args = args
+
+        self.site = pywikibot.Site()
+        self.site.login()
+
+        self.logger = logging.getLogger('generate_lta_list')
+        formatter = logging.Formatter('%(asctime)s | %(levelname)s | %(message)s')
+        stdout_handler = logging.StreamHandler(sys.stdout)
+        stdout_handler.setFormatter(formatter)
+        self.logger.addHandler(stdout_handler)
+
+    def replace_text_in_flags(self, text, flag1, flag2, ins):
+        index1 = text.index(flag1)
+        index2 = text.index(flag2)
+        return text[:index1] + flag1 + ins + text[index2:]
+
+    def main(self):
+        pages = set()
+        for page in pywikibot.pagegenerators.PrefixingPageGenerator('LTA:', includeredirects=True):
+            pages.add(page.title())
+        pages = sorted(pages)
+
+        page_list = '''<!--
+本列表由程式自動產生，任何手動修改將在下次更新時被覆蓋
+程式碼：https://github.com/xiplus-mediawiki-programs/generate-lta-list
+-->
+'''
+        for page in pages:
+            page_list += '# [[{}]]\n'.format(page)
+
+        page = pywikibot.Page(self.site, 'Wikipedia:最近更改巡查/頁面列表/LTA重新導向')
+        old_text = page.text
+
+        new_text = self.replace_text_in_flags(old_text, '<!-- list-start -->', '<!-- list-end -->', page_list)
+        if old_text == new_text:
+            self.logger.info('no changes')
+            return
+
+        new_text = self.replace_text_in_flags(new_text, '<!-- time-start -->', '<!-- time-end -->', '~~~~~')
+
+        summary = '產生列表'
+
+        if args.confirm or args.loglevel <= logging.DEBUG:
+            pywikibot.showDiff(page.text, new_text)
+            self.logger.info('summary: %s', summary)
+
+        save = True
+        if args.confirm:
+            save = pywikibot.input_yn('Save changes?', 'Y')
+        if save:
+            self.logger.info('save changes')
+            page.text = new_text
+            page.save(summary=summary, minor=False)
+        else:
+            with open('out.txt', 'w', encoding='utf8') as f:
+                f.write(new_text)
+            self.logger.info('skip save')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--confirm', action='store_true')
+    parser.add_argument('-d', '--debug', action='store_const', dest='loglevel', const=logging.DEBUG, default=logging.INFO)
+    args = parser.parse_args()
+
+    gll = GererateLtaList(args)
+    gll.logger.setLevel(args.loglevel)
+    gll.logger.debug('args: %s', args)
+    gll.main()
